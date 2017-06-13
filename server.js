29df484d1a188bcf789c2a03094ef5c13ed11626
paulowar <paulowar@umich.edu>
2017-06-13 15:23:30 -0400

    basic grouping

--- server.js
+++ server.js
@@ -1,114 +1,111 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    fs      = require('fs'),
-    app     = express(),
-    eps     = require('ejs'),
-    morgan  = require('morgan');
-    
+  fs      = require('fs'),
+  app     = express(),
+  eps     = require('ejs'),
+  morgan  = require('morgan'),
+  http    = require('http'),
+  axios   = require('axios');
+
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
-
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
-var db = null,
-    dbDetails = new Object();
-
-var initDb = function(callback) {
-  if (mongoURL == null) return;
-
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
-
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
+  ip   = '127.0.0.1' //process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+  mongoURLLabel = "";
 
 var bp = require('body-parser')
 app.use(bp.json())
 app.use(bp.urlencoded({extended: true}))
 
+var token = '8553~7G6yIufBJhp30vX9A6NYC68aHSEeBxlm0LalJI1ARASZ4UWFq9bXBhWZGx3dPZiV'
+
 app.post('/', function(req, res){
-  console.log('asd')
-  console.log(req.body)
-  res.render('index.html')
+
+  var user_dict = new Map();
+  var big_classes = []
+  axios.get('https://umich-dev.instructure.com/api/v1/courses?access_token='+token)
+    .then(function(classes){
+
+      classes = classes.data
+
+
+      for(cl in classes){
+        cl = classes[cl]
+
+        async function main(id, name){
+          resp = await axios.get('https://umich-dev.instructure.com/api/v1/courses/'+id
+            +'/students?access_token='+token)
+
+          big_classes.push({
+            name: name,
+            id: id,
+            users: resp.data
+          })
+
+          if(big_classes.length == classes.length){
+            handleClasses(big_classes, function(grouped_users){
+              console.log(grouped_users)
+              res.send(grouped_users)
+            })
+          }
+        }
+        main(cl.id, cl.name);
+        /*
+          .then(function(res){
+
+            users = res.data
+
+            for(user in users){
+              user = users[user]
+
+
+              a = user_dict.get(user.id)
+
+              console.log(a)
+
+              user_dict.set(user, user.id)
+            }
+
+          }) */
+
+      }
+    })
+    .catch(function(res){
+      console.log(res)
+    })
 })
 
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
+function handleClasses(classes, callback){
+  dictionary = new Map();
+  classes.forEach(function(cl){
+    cl.users.forEach(function(user){
+      if(dictionary.has(user.id)){
+        dictionary.get(user.id).classes.push(cl.name)
+        //console.log('dupe')
+      }
+      else{
+        //console.log(cl)
+        user.classes = [cl.name]
+        dictionary.set(user.id, user)
+      }
+    })
+  })
+
+  users = []
+  dictionary.forEach(function(item){
+    users.push(item)
+  })
+  callback(users)
+}
 
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
 });
 
 app.listen(port, ip);
